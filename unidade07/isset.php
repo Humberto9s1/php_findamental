c89aab48a1032510eea57c364a66fb61aa537386
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-01-19 02:13:03 -0300

    ISSET - Aula 306

--- unidade07/isset.php
+++ unidade07/isset.php
@@ -1,5 +1,7 @@
 <?php
-
+    $_nome = "Humberto";
+    $_telefone = "2345-0987";
+    $_fumante = null;
 ?>
 
 <!doctype html>
@@ -8,7 +10,11 @@
         <meta charset="UTF-8">
         <title>Curso PHP FUNDAMENTAL</title>
     </head>
-
+        <?php
+            echo "Existe nome? ".isset($_nome)."</br>";
+            echo "Existe telefone? ".isset($_telefone)."</br>";
+            echo "Existe fumante? ".isset($_fumante)."</br>";
+        ?>
     <body>
     </body>
 </html>
