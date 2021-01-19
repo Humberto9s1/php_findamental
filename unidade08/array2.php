7bb690bf58346856bb29a1412175d006230deda4
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-01-19 03:22:11 -0300

    Funções para Array - Aula 309

--- unidade08/array2.php
+++ unidade08/array2.php
@@ -1,5 +1,7 @@
 <?php
-
+    $_megasena = array(47,29,42,04,27,21);
+    sort($_megasena); //ordem Crescente
+    //rsort($_megasena); ordem Decrescente -- r de reverso
 ?>
 
 <!doctype html>
@@ -10,9 +12,15 @@
     </head>
 
     <body>
-        <?php 
-            $_1telefone = "Matheus Fontenelle"; 
-            echo $_1telefone;
+        <?php
+            echo "Documentação de Array -- https://www.php.net/manual/pt_BR/book.array.php " . "</br>" . "</br>";
+            echo "Menor valor " . min($_megasena) . "</br>";
+            echo "Maior valor " . max($_megasena) . "</br>";
         ?>
+        <pre>
+            <?php 
+                print_r($_megasena);
+            ?>
+        </pre>
     </body>
 </html>
