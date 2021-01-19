c4df7fed5d475270171fcc4e6d7874b88c930005
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-01-19 01:41:30 -0300

    NULL - Aula 305

--- unidade07/null.php
+++ unidade07/null.php
@@ -1,5 +1,5 @@
 <?php
-
+    $_nome = null;
 ?>
 
 <!doctype html>
@@ -8,7 +8,10 @@
         <meta charset="UTF-8">
         <title>Curso PHP FUNDAMENTAL</title>
     </head>
-
+        <?php
+            echo "nome = ".$_nome."</br>";
+            echo "nome is null? ".is_null($_nome)."</br>";
+        ?>
     <body>
     </body>
 </html>
