1bbd56acd9d380c20235b22bfced85460ee96d4d
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-01-19 05:21:52 -0300

    Operadores lógicos - exercício 3 - Aula 320

--- unidade09/acao-condicional6.php
+++ unidade09/acao-condicional6.php
@@ -7,7 +7,22 @@
     </head>
     <body>
         <?php 
+            $_dia = "domingo";
+            $_sexo = 19;
+            $_idade = "feminino";
 
+            if ($_dia == "sabado" || $_dia == "domingo") {
+                echo "Pode descansar." . "</br>";
+            } else {
+                echo "Dia de trabalho." . "</br>";
+            }
+            
+
+            if ($_idade >= 18 && $_sexo == "feminino") {
+                echo "Pode entrar na festa.";
+            } else {
+                echo "Não pode entrar na festa.";
+            }
         ?>
     </body>
 </html>
