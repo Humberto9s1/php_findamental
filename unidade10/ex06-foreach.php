852dfaad9fc4de0d54daec7d446dc04dcb97e587
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-02-02 04:31:36 -0300

    Foreach - Exemplo 2 - Aula 327

--- unidade10/ex06-foreach.php
+++ unidade10/ex06-foreach.php
@@ -6,7 +6,12 @@
 </head>
 <body>
     <?php                    
-        
+        $_agenda = array("nome" => "Humberto",
+                        "telefone" => "999-888-777",
+                        "salario" => 1000.50);
+        foreach( $_agenda as $_contato => $_valor ){
+            echo $_contato . ": " . $_valor . "</br>";
+        }
     ?>
 </body>
 </html>
