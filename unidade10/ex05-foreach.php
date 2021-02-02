2df030720775990f8f528d03e5c3341f91a8d5ba
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-02-02 04:24:56 -0300

    Foreach - Exemplo 1 - Aula 326

--- unidade10/ex05-foreach.php
+++ unidade10/ex05-foreach.php
@@ -6,7 +6,15 @@
 </head>
 <body>
     <?php                    
-        $_salada = array("","","");
+        $_salada = array("laranja","uva","abacate", "limão", "mamão");
+
+        for($_i = 0; $_i <3 ;$_i++){
+            echo $_salada[$_i] . "</br>";
+        }
+
+        foreach($_salada as $_fruta){
+            echo $_fruta . "</br>";
+        }
     ?>
 </body>
 </html>
