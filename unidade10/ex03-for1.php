ce8d48b159f82fbdc290b6426f76b3194e31d951
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-02-02 04:14:49 -0300

    For - Exemplo 1 - Aula 324

--- unidade10/ex03-for1.php
+++ unidade10/ex03-for1.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>CURSO PHP FUNDAMENTAL</title>
 </head>
+
 <body>
-    <?php                    
+    <?php
+    
+    for( $_contador = 1 ; $_contador < 7 ; $_contador++ ){
+        $_sorteio = rand(1, 60);
+        echo $_sorteio . " ";
+    }
 
     ?>
 </body>
+
 </html>
