cf3d5e07d23513b39c81d616f594a44bc1ecde92
HUMBERTO MARTINS <humbertosant.mart@gmail.com>
2021-02-10 00:05:18 -0300

    DIFF - Aula 338

--- unidade12/ex05-diff.php
+++ unidade12/ex05-diff.php
@@ -6,5 +6,42 @@
     </head>
 
     <body>
+        
+        <?php
+            $data1 = new DateTime('2020-01-01');
+            $data2 = new DateTime('2021-02-08');
+            $intervalo = $data1->diff($data2);
+        ?>
+
+        <pre>
+            <?php
+                print_r($intervalo);
+            ?>
+        </pre>
+        
+        <pre>
+            <?php
+                print_r($intervalo->format('%y'))."ano"."<br>";
+            ?>
+        </pre>
+        
+        <pre>
+            <?php
+                print_r($intervalo->format('%m'))."mês"."<br>";
+            ?>
+        </pre>
+        
+        <pre>
+            <?php
+                print_r($intervalo->format('%d'))."dia "."<br>";
+            ?>
+        </pre>
+
+        <pre>
+            <?php
+                print_r($intervalo->format('%a'))."intervalo entre as duas datas"."<br>";
+            ?>
+        </pre>
+
     </body>
 </html>
